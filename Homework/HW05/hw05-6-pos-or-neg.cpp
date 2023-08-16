6bd50be0d7cc072d09cb86aabd01afb40cef8b85
teeratapdeesussus <deesussus999@gmail.com>
2023-08-17 05:25:33 +0700

    hw5-6

--- Homework/HW05/hw05-6-pos-or-neg.cpp
+++ Homework/HW05/hw05-6-pos-or-neg.cpp
@@ -16,3 +16,18 @@
         Negative
 
 */
+#include <stdio.h>
+
+int main(){
+    int num ;
+    printf( "Input :\n" ) ;
+    scanf( "%d", &num ) ;
+    
+    if( num > 0 ) {
+        printf( "Positive" ) ;
+    } else {
+        printf( "Negative" ) ;
+    }
+
+    return 0 ;
+}
